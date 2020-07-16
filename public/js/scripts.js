3861b78e5e23797d0f01c6be9d316c4dbcfc143c
Andrei Fedchenko <33954126+Wh1teNinja@users.noreply.github.com>
2020-07-16 19:23:43 -0400

    Added email sender

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,10 +1,12 @@
 window.onload = () => {
   let menuButton = document.querySelector("#menu-button");
   menuButton.onclick = () => {
-    console.log(1);
+    let sheet = document.styleSheets[0];
+    let rules = sheet.cssRules || sheet.rules;
+    sheet.innerHTML = ""
     let menu = document.querySelector("#header-menu");
-    if (menu.style.display === "flex") {
-      menu.style.display = "none";
-    } else menu.style.display = "flex";
+    if (menu.style.display === "none") {
+      menu.style.display = "flex";
+    } else menu.style.display = "none";
   };
 };
